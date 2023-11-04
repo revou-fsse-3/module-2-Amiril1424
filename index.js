4b1552644cd3d0f46b533245dd12aec0dd695283
Amiril Mukminin <amiril.mukminin1424@gmail.com>
2023-11-04 13:39:26 +0700

    Tugas Week3 Module 2

---
+++ index.js
@@ -0,0 +1,9 @@
+// Function to give alert when click submit
+function myFunction() {
+    alert("Thank You for Joining Us!");
+}
+
+function ubahMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
